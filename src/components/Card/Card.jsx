d86bc6c85c2c5bc8df2211c902b421c65a8d7966
R8BY <vansosnova@mail.ru>
2022-02-12 14:52:22 +0300

    Было весело

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import classes from './Card.module.css';
 import avatar from '../../images/image-placeholder.png';
 
+function addPlaceholder(e) {
+    e.target.src = avatar;
+}
+
 const Card = ({poster, title, year, id, type}) => {
     return (
         <div className={classes.card}>
-            <img className={classes.poster} src={poster} alt="poster"/>
+            <img className={classes.poster} src={poster} onError={addPlaceholder} alt="poster"/>
             <ul className={classes.infoCard}>
                 <li>Name: {title}</li>
                 <li>Year: {year}</li>
